132c7d65c980b5e8d9e957286b4274cbf1d028ef
David Soards <davidysoards@gmail.com>
2019-12-08 20:23:03 -0500

    add hints

---
+++ main.js
@@ -0,0 +1,24 @@
+// create a reassignable variable to hold the current slide number
+
+// create a function called showSlide that takes 1 argument called slideNumber
+function showSlide(slideNumber) {
+  // select all DOM elements with the class 'slide' and store them in a variable
+  // select all DOM elements with the class 'dot' and store them in a variable
+  // if the slide number is greater than total number of slides, set it to 1
+  // if the slide number is less than 0, set it to the number of the last slide
+  // loop over all of the slides and set them to display: none
+  // loop over all the dots and remove the 'active' class
+  // set the current slide to display: block
+  // add the 'active' class to the current dot
+}
+
+// write a function to increase the current slide number by 1 and show the new slide
+function increaseSlide() {}
+
+// write a function to decrease the current slide number by 1 and show the new slide
+function decreaseSlide() {}
+
+// write a function to set the current slide to a specific number and show the new slideg
+function specificSlide(slideNumber) {}
+
+// don't forget to call your showSlide function on initial page load
