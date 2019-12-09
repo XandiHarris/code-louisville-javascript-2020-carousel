78d696e9a9a4157a29d49d1df982bf6b893650b8
David Soards <davidysoards@gmail.com>
2019-12-08 20:26:34 -0500

    fix typos

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function showSlide(slideNumber) {
   // select all DOM elements with the class 'slide' and store them in a variable
   // select all DOM elements with the class 'dot' and store them in a variable
   // if the slide number is greater than total number of slides, set it to 1
-  // if the slide number is less than 0, set it to the number of the last slide
+  // if the slide number is less than 1, set it to the number of the last slide
   // loop over all of the slides and set them to display: none
   // loop over all the dots and remove the 'active' class
   // set the current slide to display: block
@@ -18,7 +18,7 @@ function increaseSlide() {}
 // write a function to decrease the current slide number by 1 and show the new slide
 function decreaseSlide() {}
 
-// write a function to set the current slide to a specific number and show the new slideg
+// write a function to set the current slide to a specific number and show the new slide
 function specificSlide(slideNumber) {}
 
 // don't forget to call your showSlide function on initial page load
